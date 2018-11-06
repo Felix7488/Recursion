cbb9486b5926979654f26df224f78af83a4e79b8
Felix7488 <43891774+Felix7488@users.noreply.github.com>
2018-11-06 11:24:38 -0800

    ghdfhldsfhbdsflj

--- Recursion/recursionDriver.java
+++ Recursion/recursionDriver.java
@@ -1,31 +1,40 @@
-
 public class recursionDriver
 {
- 
-     public static int count(int n)
+   
+    public static int count(int n)
     {
-        // Base case AKA Stop Condition
-         if (n==0)
+        if (n==0)
              return 0; 
  
          if (n==1)
              return 4;
  
-         return 4+count(n-1); 
-    } 
- 
- 
- 
-     public static void main(String[] args)
-     {
-     /*
-     * Count the number of legs 10 elephants have
-     */ 
-    //Task 1
-    int counted=0;
-     counted = count(10);
-     System.out.println(counted);
-   
+         return 4+count(n-1);   
+    }
+    
+    
+    
+    public static void main(String[] args)
+    {
+         /*
+          * Count 
+          * 1. Number of legs  10 elephant have
+          * 2. Number of students in a school if each
+          *    class has 20 students and there are 12 rooms
+          * 3. Number of fingers if there are 8 people
+          * 4. Find the sum of odd numbers from 0 to N
+          * 5. Find the sum of even numbers from 0 to N
+     
+          * 
+          */   
+         int counted=0;
+
+         // Task 1.
+         counted = count(10);
+         System.out.println(counted);
+         
+         //Task 2
+         
 
     }
 }
